bc0678548e20c6908bab2cecfc7c7f06f4012b67
katyeheisenberg <chimmi10kate@mail.ru>
2023-02-01 03:51:31 +0300

    made contact section and added photos

--- pages/index.js
+++ pages/index.js
@@ -2,46 +2,41 @@
 import Head from "next/head";
 import {
   AiFillTwitterCircle,
-  AiFillLinkedin,
-  AiFillYoutube,
   AiFillInstagram,
 } from "react-icons/ai";
-import { BsFillMoonStarsFill } from "react-icons/bs";
-import { useState } from "react";
 import Me from "../public/meem.png";
-import code from "../public/code.png";
-import design from "../public/design.png";
-import consulting from "../public/consulting.png";
 import Image from "next/image";
-import web1 from "../public/web1.png";
-import web2 from "../public/web2.png";
-import web3 from "../public/web3.png";
-import web4 from "../public/web4.png";
-import web5 from "../public/web5.png";
-import web6 from "../public/web6.png";
+import img1 from "../public/6.jpg";
+import img2 from "../public/5.jpg";
+import img4 from "../public/1.jpg";
+import img5 from "../public/2.jpg";
+import img6 from "../public/3.jpg";
+import img7 from "../public/4.JPG";
+import img8 from "../public/building.jpg";
+import Mar1 from "../public/Marina1.jpg";
+import Mar2 from "../public/Marina2.jpg";
+import Mar3 from "../public/Marina3.jpg";
+import Mar4 from "../public/Marina4.jpg";
 import logo3 from "../public/logo3.svg";
 
 export default function Home() {
-  const [darkMode, setDarkMode] = useState(false);
-
   return (
-    <div className={darkMode ? "dark" : ""}>
+    <div>
       <Head>
         <title>Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className=" bg-white px-10 md:px-20 lg:px-40">
         <section className="min-h-screen">
-          <div className="text-center p-15 py-10">
+          <div className="text-center p-5 py-10">
             <Image src={logo3} alt="" />
-            <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
-              Photographer
+            <h3 className="text-3xl py-10 md:text-3xl font-semibold">
+              Film photographer.
             </h3>
-            <p className="text-md py-4 leading-8 text-gray-800 max-w-sm mx-auto md:text-xl">
-              Shooting mostly portraits of people, street photos and photos of
-              nature.
+            <p className="text-sm py-4 text-gray-800 max-w-sm mx-auto md:text-xl font-thin">
+              Portraits. Street photography. Nature photography.
             </p>
-            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
+            <div className="text-5xl flex justify-center gap-16 py-10 text-gray-600">
               <a
                 href="https://www.instagram.com/katyathegreatest/"
                 target="_blank"
@@ -55,144 +50,153 @@ export default function Home() {
                 <AiFillTwitterCircle />
               </a>
             </div>
-            <div className="mx-auto rounded-full w-60 h-60 relative overflow-hidden mt-10 md:h-70 md:w-70 ">
+            <div className="mx-auto rounded-full w-60 h-60 relative overflow-hidden mt-10 md:h-70 md:w-70">
               <Image src={Me} layout="fill" objectFit="cover" />
             </div>
           </div>
         </section>
-        <section>
+        <section className="py-1">
           <div>
-            <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
-            </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
-            </p>
-          </div>
-          <div className="lg:flex gap-10">
-            <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
-              <Image src={design} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2  ">
-                Beautiful Designs
-              </h3>
-              <p className="py-2">
-                Creating elegant designs suited for your needs following core
-                design theory.
-              </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
-            </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={code} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 ">
-                Code your dream project
-              </h3>
-              <p className="py-2">
-                Do you have an idea for your next great website? Let's make it a
-                reality.
-              </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
-            </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={consulting} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
-              <p className="py-2">
-                Are you interested in feedback for your current project? I can
-                give you tips and tricks to level it up.
-              </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
-            </div>
-          </div>
-        </section>
-        <section className="py-10">
-          <div>
-            <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
-            </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
+            <h3 className="text-3xl text-center font-semibold">Portofolio</h3>
+            <p className="text-xl py-2 leading-8 text-center text-gray-800 font-thin">
+              Collection of my recent photos.
             </p>
           </div>
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
             <div className="basis-1/3 flex-1 ">
               <Image
                 className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
+                width={"80%"}
+                height={"120%"}
                 layout="responsive"
-                src={web1}
+                src={img1}
               />
             </div>
             <div className="basis-1/3 flex-1">
               <Image
                 className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
+                width={"80%"}
+                height={"120%"}
                 layout="responsive"
-                src={web2}
+                src={img2}
               />
             </div>
             <div className="basis-1/3 flex-1">
               <Image
                 className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
+                width={"80%"}
+                height={"120%"}
                 layout="responsive"
-                src={web3}
+                src={img4}
               />
             </div>
             <div className="basis-1/3 flex-1">
               <Image
                 className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
+                width={"80%"}
+                height={"120%"}
                 layout="responsive"
-                src={web4}
+                src={img5}
               />
             </div>
             <div className="basis-1/3 flex-1">
               <Image
                 className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
+                width={"80%"}
+                height={"120%"}
                 layout="responsive"
-                src={web5}
+                src={img6}
               />
             </div>
             <div className="basis-1/3 flex-1">
               <Image
                 className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
+                width={"80%"}
+                height={"120%"}
                 layout="responsive"
-                src={web6}
+                src={img7}
               />
             </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"80%"}
+                height={"120%"}
+                layout="responsive"
+                src={Mar1}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"80%"}
+                height={"120%"}
+                layout="responsive"
+                src={Mar2}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"80%"}
+                height={"120%"}
+                layout="responsive"
+                src={Mar3}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"80%"}
+                height={"120%"}
+                layout="responsive"
+                src={Mar4}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"110%"}
+                height={"30%"}
+                layout="responsive"
+                src={img8}
+              />
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <section className="text-gray-700">
+              <div className="px-5 py-24 mx-auto">
+                <div className="flex flex-col text-center w-full mb-7">
+                  <h1 className="sm:text-3xl font-thin mb-4 text-gray-900">
+                    Contact Me
+                  </h1>
+                </div>
+                <form className="flex flex-col gap-y-4">
+                  <div className="flex gap-x-10">
+                    <input
+                      className="outline-none border-b border-b-primary h-[60px] bg-transparent w-full pl-3 placeholder:text-[#757879]"
+                      type="text"
+                      placeholder="Your name"
+                    />
+                    <input
+                      className="outline-none border-b border-b-primary h-[60px] bg-transparent w-full pl-3 placeholder:text-[#757879]"
+                      type="text"
+                      placeholder="Your email address"
+                    />
+                  </div>
+                  <input
+                    className="outline-none border-b border-b-primary h-[60px] bg-transparent w-full pl-3 placeholder:text-[#757879]"
+                    type="text"
+                    placeholder="Your message"
+                  />
+                  <button className="flex mx-auto text-white bg-cyan-700 border-0 py-3 my-7 px-8 focus:outline-none hover:bg-blue-400 rounded text-lg">
+                    Send
+                  </button>
+                </form>
+              </div>
+            </section>
           </div>
         </section>
       </main>
